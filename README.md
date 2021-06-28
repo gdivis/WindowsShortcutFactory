9e228c305d79545564e9787a8ad0c617cd1df115
Greg Divis, Inedo <gdivis@inedo.com>
2021-06-28 11:07:18 -0400

    Add project files.

---
+++ README.md
@@ -0,0 +1,15 @@
+# WindowsShortcutFactory
+
+This is a simple .NET library for creating or modifying Windows shortcut files. It has no dependencies, and doesn't use .NET COM Interop or Windows Script Host, instead calling into the native IShellLink interface directly.
+
+## Creating a Shortcut
+
+This example creates a shortcut to `C:\my\program\target.exe`:
+
+    using var shortcut = new WindowsShortcut
+	{
+	    Path = @"C:\my\program\target.exe",
+		Description = "Just a simple shortcut to target.exe."
+	};
+	
+	shortcut.Save(@"C:\temp\MyShortcut.lnk");
---
+++ WindowsShortcutFactory.Tests/WindowsShortcutTests.cs
@@ -0,0 +1,82 @@
+using System.Diagnostics;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WindowsShortcutFactory.Tests
+{
+    [TestClass]
+    public class WindowsShortcutTests
+    {
+        [TestMethod]
+        public void TestPath()
+        {
+            using var s = new WindowsShortcut();
+            Assert.IsNull(s.Path);
+            s.Path = @"C:\my\test\path.exe";
+            Assert.AreEqual(@"C:\my\test\path.exe", s.Path);
+        }
+        [TestMethod]
+        public void TestDescription()
+        {
+            using var s = new WindowsShortcut();
+            Assert.AreEqual(string.Empty, s.Description);
+            s.Description = "description";
+            Assert.AreEqual("description", s.Description);
+        }
+        [TestMethod]
+        public void TestArguments()
+        {
+            using var s = new WindowsShortcut();
+            Assert.AreEqual(string.Empty, s.Arguments);
+            s.Arguments = "my arguments";
+            Assert.AreEqual("my arguments", s.Arguments);
+        }
+        [TestMethod]
+        public void TestWorkingDirectory()
+        {
+            using var s = new WindowsShortcut();
+            Assert.AreEqual(string.Empty, s.WorkingDirectory);
+            s.WorkingDirectory = @"C:\my\test\path";
+            Assert.AreEqual(@"C:\my\test\path", s.WorkingDirectory);
+        }
+        [TestMethod]
+        public void TestShowCommand()
+        {
+            using var s = new WindowsShortcut();
+            Assert.AreEqual(ProcessWindowStyle.Normal, s.ShowCommand);
+            s.ShowCommand = ProcessWindowStyle.Maximized;
+            Assert.AreEqual(ProcessWindowStyle.Maximized, s.ShowCommand);
+        }
+        [TestMethod]
+        public void TestIconLocation()
+        {
+            using var s = new WindowsShortcut();
+            Assert.AreEqual(IconLocation.None, s.IconLocation);
+
+            var loc = new IconLocation(@"C:\dir\icon.ico");
+            s.IconLocation = loc;
+            Assert.AreEqual(loc, s.IconLocation);
+
+            loc = new IconLocation(@"C:\dir\icons.dll", 10);
+            s.IconLocation = loc;
+            Assert.AreEqual(loc, s.IconLocation);
+        }
+        [TestMethod]
+        public void TestSave()
+        {
+            using var s = new WindowsShortcut();
+            s.Path = @"C:\my\test\path.exe";
+
+            var fileName = Path.GetTempFileName();
+            try
+            {
+                s.Save(fileName);
+                Assert.IsTrue(File.Exists(fileName));
+            }
+            finally
+            {
+                File.Delete(fileName);
+            }
+        }
+    }
+}
